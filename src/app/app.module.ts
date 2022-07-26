376665ef6aa8a6cf9ec080f195946e68d61dd9fd
reggie <reggie.skillman34@icloud.com>
2022-07-26 10:51:30 -0400

    Home and Settings added to Routing

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,19 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
-
+import { HomeComponent } from './pages/home/home.component';
+import { SettingsComponent } from './pages/settings/settings.component';
+import { AppRoutingModule } from './app.routing.module';
 @NgModule({
   declarations: [
     AppComponent,
+    HomeComponent,
+    SettingsComponent,
   ],
+
   imports: [
-    BrowserModule
+    BrowserModule,
+    AppRoutingModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/app.routing.module.ts
+++ src/app/app.routing.module.ts
@@ -1,9 +1,11 @@
 import { NgModule } from "@angular/core";
 import { RouterModule, Routes } from "@angular/router";
-
+import { HomeComponent } from "./pages/home/home.component";
+import { SettingsComponent } from "./pages/settings/settings.component";
 
 const routes : Routes = [
-
+{ path: '', component: HomeComponent},
+{ path: 'settings',component: SettingsComponent}
 ];
 
 @NgModule ({
@@ -11,4 +13,4 @@ const routes : Routes = [
     exports: [RouterModule]
 })
 
-export class AppRoutingModule{};
+export class AppRoutingModule{}
