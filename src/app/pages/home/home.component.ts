22c53e5d0c1213d8dc4f9ed4e627b78916c75530
reggie <reggie.skillman34@icloud.com>
2022-08-03 14:02:02 -0400

    CSS complete

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,12 +1,16 @@
 import { Component, OnInit } from '@angular/core';
-
+import { faCloudShowersHeavy } from '@fortawesome/free-solid-svg-icons';
+import { faSun } from '@fortawesome/free-solid-svg-icons';
+import { faGear } from '@fortawesome/free-solid-svg-icons';
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.scss']
 })
 export class HomeComponent implements OnInit {
-
+  faSun= faSun;
+  faCloudShowersHeavy = faCloudShowersHeavy;
+  faGear = faGear;
   constructor() {}
 
   ngOnInit(): void {
